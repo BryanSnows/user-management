d287ba0dbad2cbbb5ae9e9c077e58eee21639696
bryan.pinto <bryan.pinto@conecthus.org.br>
2024-01-09 15:55:58 -0400

    fix: bug fix

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -1,11 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    private readonly ILoggerFactory _loggerFactory;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, ILoggerFactory loggerFactory)
+        : base(options)
+    {
+        _loggerFactory = loggerFactory;
+    }
 
     public DbSet<Profile> Profiles { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<ProfileTransaction> ProfileTransactions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLoggerFactory(_loggerFactory)
+                      .EnableSensitiveDataLogging()
+                      .UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=3663"); 
+    }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
 //* Configurar a conexão com o banco de dados
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseNpgsql(connectionString);
+    options.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=3663");
 });
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -17,10 +17,17 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "User Management API", Version = "v1" });
 });
 
-
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+
+//* Configurar LoggerFactory
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole(); //* Pode adicionar outros provedores de log aqui, se necessário
+});
+
+builder.Services.AddSingleton(loggerFactory);
 
 var app = builder.Build();
 
@@ -35,6 +42,4 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-
-app.MapControllers();
 app.Run();
